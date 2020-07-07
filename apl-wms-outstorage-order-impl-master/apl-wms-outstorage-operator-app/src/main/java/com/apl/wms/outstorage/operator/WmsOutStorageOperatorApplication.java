5e65ea72eb28d45f78a5a1d40a65172fc4d72123
arranchen <arran@aplun.net>
2020-07-08 00:15:15 +0800

    apl-datasource

--- apl-wms-outstorage-order-impl-master/apl-wms-outstorage-operator-app/src/main/java/com/apl/wms/outstorage/operator/WmsOutStorageOperatorApplication.java
+++ apl-wms-outstorage-order-impl-master/apl-wms-outstorage-operator-app/src/main/java/com/apl/wms/outstorage/operator/WmsOutStorageOperatorApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages = {"com.apl.wms.outstorage.operator.*" ,  "com.apl.wms.outstorage.order.*" , "com.apl.wms.warehouse.lib.*", "com.apl.lib", "com.apl.datasource",  "com.apl.lib.handler"}, exclude = {DataSourceAutoConfiguration.class})
@@ -18,8 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class WmsOutStorageOperatorApplication {
     public static void main(String[] args) {
 
-        //com.apl.lib.datasource.DataSourceConfig
-        //com.apl.lib.datasource.DynamicDataSource
+        //com.apl.datasource.DataSourceConfig
 
         SpringApplication.run(WmsOutStorageOperatorApplication.class , args);
     }
