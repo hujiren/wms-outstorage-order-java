9030f0254014cda33aaef9d5aa1fe992df6ecaa1
arranchen <arran@aplun.net>
2020-07-17 09:25:59 +0800

    apl.db.datasource

--- apl-wms-outstorage-order-impl-master/apl-wms-outstorage-operator-app/src/main/java/com/apl/wms/outstorage/operator/WmsOutStorageOperatorApplication.java
+++ apl-wms-outstorage-order-impl-master/apl-wms-outstorage-operator-app/src/main/java/com/apl/wms/outstorage/operator/WmsOutStorageOperatorApplication.java
@@ -16,16 +16,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
                 "com.apl.db.datasource",
                 "com.apl.amqp",
                 "com.apl.lib",
-                "com.apl.lib.handler"}, exclude = {DataSourceAutoConfiguration.class})
-@EnableFeignClients(basePackages = {"com.apl.wms.outstorage.order.lib.feign", "com.apl.wms.warehouse.lib.feign", "com.apl.sys.lib.feign"})
+                "com.apl.lib.handler"},
+        exclude = {DataSourceAutoConfiguration.class})
+@EnableFeignClients(
+        basePackages = {"com.apl.wms.outstorage.order.lib.feign",
+                "com.apl.wms.warehouse.lib.feign",
+                "com.apl.sys.lib.feign"})
 @MapperScan({"com.apl.wms.outstorage.operator.dao" , "com.apl.wms.outstorage.order.dao"})
 @EnableDiscoveryClient
 @EnableSwagger2
 public class WmsOutStorageOperatorApplication {
     public static void main(String[] args) {
-
-        //com.apl.db.datasource.DataSourceConfig
-        //com.apl.lib.config.RedisConfig
 
         SpringApplication.run(WmsOutStorageOperatorApplication.class , args);
     }
