ed4ba5340d25a25a93a2747430bba23434180825
arranchen <arran@aplun.net>
2020-09-14 11:47:48 +0800

    apl-shardingjdbc-mybatis

--- apl-wms-outstorage-order-impl-master/apl-wms-outstorage-operator-app/src/main/java/com/apl/wms/outstorage/operator/WmsOutStorageOperatorApplication.java
+++ apl-wms-outstorage-order-impl-master/apl-wms-outstorage-operator-app/src/main/java/com/apl/wms/outstorage/operator/WmsOutStorageOperatorApplication.java
@@ -5,7 +5,6 @@ import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
